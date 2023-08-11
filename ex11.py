1747fc7c65cedd11f35d4a228de7b6aa4150a815
misosucky <126988962+Misosucky@users.noreply.github.com>
2023-08-10 21:03:38 -0400

    added notes

--- ex11.py
+++ ex11.py
@@ -1,8 +1,14 @@
+#Asks user to enter age, end= prompt Var
 print("How old are you?", end = ' ')
+#Var defined by user
 age = input ()
+#asks the user to enter height, end ' ' prompts var
 print("How tall are you?", end = ' ')
+#var defined by user
 height = input ()
+#asks for input via print string
 print("How much do you weigh?", end = ' ')
+#var defined by user
 weight = input()
-
+#f string which combines all Var into one statement
 print(f"So, you're {age} old, {height} tall, and {weight} heavy.")
