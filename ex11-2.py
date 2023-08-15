83648be4054884f4d1b5f2c7826c3aaf78cbbffa
misosucky <126988962+Misosucky@users.noreply.github.com>
2023-08-14 20:55:58 -0400

    created prints, strings, and vars

---
+++ ex11-2.py
@@ -0,0 +1,16 @@
+print("What is the dumbest thing you've spent moeny on?" , end =' ')
+moeny_spent = ()
+
+print("Interesting..")
+
+print("Did you regret it?", end = ' ')
+regret = ()
+
+print("Well, what's the smartest thing you've spent money on ??")
+smart = ()
+
+print("W O A H! How much was that exactly?!", end = ' ')
+
+cost = ()
+
+print (f"Well had you avoided your first mistake you could have only spent {moeny_spent - cost} on your smartest purchase!")
