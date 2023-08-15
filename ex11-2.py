b9f10e7102b91c51d502fa556c042f8b00d43e8d
misosucky <126988962+Misosucky@users.noreply.github.com>
2023-08-14 20:57:25 -0400

    added "input"

--- ex11-2.py
+++ ex11-2.py
@@ -1,16 +1,16 @@
 print("What is the dumbest thing you've spent moeny on?" , end =' ')
-moeny_spent = ()
+moeny_spent = input ()
 
 print("Interesting..")
 
 print("Did you regret it?", end = ' ')
-regret = ()
+regret = input()
 
 print("Well, what's the smartest thing you've spent money on ??")
-smart = ()
+smart = input()
 
 print("W O A H! How much was that exactly?!", end = ' ')
 
-cost = ()
+cost = input()
 
 print (f"Well had you avoided your first mistake you could have only spent {moeny_spent - cost} on your smartest purchase!")
