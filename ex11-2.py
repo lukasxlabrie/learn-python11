80b381ac9f05171bb652de58cb0f8156db68c7da
misosucky <126988962+Misosucky@users.noreply.github.com>
2023-08-27 14:00:59 -0400

    added print and notes

--- ex11-2.py
+++ ex11-2.py
@@ -5,3 +5,6 @@ first_name = input ()
 #this line asks user to input the name of a street
 print ("What is the name of the street you grew up on?", end = '  ')
 street_name = input ()
+
+#this line prints both inputs and creates a sentence
+print (first_name, street_name, "Is your alter ego!")
