ee48a8396900c0abb065ca00f74a91ca84a712f1
misosucky <126988962+Misosucky@users.noreply.github.com>
2023-08-27 13:59:39 -0400

    added street var and notes

--- ex11-2.py
+++ ex11-2.py
@@ -1,2 +1,7 @@
+#this line asks user to input first name
 print("What is your first name?", end = ' ')
 first_name = input ()
+
+#this line asks user to input the name of a street
+print ("What is the name of the street you grew up on?", end = '  ')
+street_name = input ()
